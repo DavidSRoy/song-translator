7f1d41bca4ac0a26beea864cd1774858b6be6441
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2022-04-18 22:07:50 -0700

    Feature/evaluation (#1)
    * set up system to get number of syllables for a given word
    
    * added evaluation for spanish
    
    * incorporated bleu score

---
+++ evaluation.py
@@ -0,0 +1,44 @@
+import pronouncing as pro
+from syltippy import syllabize
+from nltk.translate.bleu_score import sentence_bleu
+
+
+SYLLABLE_WEIGHT = 0.6
+BLEU_WEIGHT = 0.4
+SYLLABLE_THRESHOLD = 10 #to be removed
+
+def getNumSyllablesEN(sentence):
+    lst = sentence.split(' ')
+    total_ct = 0
+    for word in lst:
+        pro_list = pro.phones_for_word(word)
+        total_ct += pro.syllable_count(pro_list[0])
+    return total_ct
+
+def getNumSyllablesES(word):
+    return len(syllabize(word)[0])
+
+def getBleuScore(reference, candidate):
+    return sentence_bleu([reference], candidate)
+
+def evaluate(sentence_en, sentence_es, sentence_es_actual):
+    syllable_score = getNumSyllablesEN(sentence_en) - getNumSyllablesES(sentence_es)
+    bleu_score = getBleuScore(sentence_es, sentence_es_actual)
+    
+    print(f'Syllable Score: {syllable_score}')
+    print(f'Bleu Score: {bleu_score}')
+    return (-pow(syllable_score,2) * SYLLABLE_WEIGHT + SYLLABLE_THRESHOLD) + bleu_score * BLEU_WEIGHT
+
+while True:
+    sentence_en = input("Enter an English sentence: ")
+    sentence_es = input("Enter a candidate Spanish sentence: ")
+    sentence_es_actual = input("Enter the actual Spanish translation")
+    score = evaluate(sentence_en, sentence_es, sentence_es_actual)
+    print(f'Score: {score}')
+
+
+# while True:
+#     word = input("Enter a word: ")
+#     num = getNumSyllablesES(word)
+#     print(f'Number of syllables: {num}')
+    
